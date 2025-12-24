be0ac53dd749445c7cece9372185510448132d3c
just-jul <businessbyjul@gmail.com>
2025-12-24 13:05:46 +0100

    Correct login logic

--- src/main/java/com/projecttracker/UI.java
+++ src/main/java/com/projecttracker/UI.java
@@ -12,14 +12,33 @@ public class UI {
         this.userList = new ArrayList<>();
     }
 
-    public void start(){
+    public void logIn(){
         System.out.println("Enter surname: ");
         String surname = scanner.nextLine();
 
         for(User u : this.userList){
-            if(u.getSurname() == surname){
+            if(u.getSurname().equalsIgnoreCase(surname) && u.getSurname() != null){ // case-insesnitive check 
                 System.out.println("Welcome!");
-                System.out.println("-----University Calendar-----");
+                start();
+                break;
+            }
+        }
+    }
+    public void start(){
+        System.out.println("-----University Calendar-----");
+
+        int choice = 0;
+
+        while(true){
+            System.out.println("MENU: ");
+            System.out.println("1. Show all.");
+            System.out.println("2. Add task.");
+            System.out.println("3. Find task.");
+            System.out.println("4. Add exam.");
+            System.out.println("5. Add study session.");
+            System.out.println("6. Exit.");
+            if(choice == 6){
+                break;
             }
         }
     }
