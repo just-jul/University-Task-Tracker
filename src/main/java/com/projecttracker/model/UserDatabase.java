127afe0bdb0a2a4434cf7987d644f2ac2e95e32b
just-jul <businessbyjul@gmail.com>
2026-01-08 20:53:19 +0100

    Add function writing User data into JSON file

--- src/main/java/com/projecttracker/model/UserDatabase.java
+++ src/main/java/com/projecttracker/model/UserDatabase.java
@@ -1,6 +1,9 @@
 package com.projecttracker.model;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -11,21 +14,27 @@ import com.google.gson.stream.JsonReader;
 
 
 public class UserDatabase {
+    private User user;
     private static final String JSONFILEPATH = "data/<surname>.json";
 
-    public UserDatabase(User user){
-
+    public UserDatabase(){
     }
 
 
-    public void toJSON(User user){
-        Gson gson = new Gson();
+    // writing User data into JSON file 
+    public void save(User user){
 
-        String json = gson.toJson(user);
+        String filePath = "data/"+user.getSurname()+".json";
 
-
-
+        try(BufferedWriter br = new BufferedWriter(new FileWriter(filePath))){
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            br.write(gson.toJson(user));
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
     }
+
+    // loading JSON file
     public User loadFile(String surname){
         String filePath = "data/"+surname+".json";
          try (BufferedReader br = new BufferedReader(new FileReader(filePath))){
@@ -34,14 +43,10 @@ public class UserDatabase {
             User user = gson.fromJson(br, User.class);
 
             return user;
-
-
-
         }catch (Exception e){
             System.out.println(e.getMessage());
             return null;
         }
-   
     }
     
     public void addClass(UniClass c){
@@ -50,7 +55,7 @@ public class UserDatabase {
     public void addTask(UniTask task){
 
     }
-    public void addExam(UNiExam exam){
+    public void addExam(UniExam exam){
 
     }
     public void addStudySession(StudySession session){
